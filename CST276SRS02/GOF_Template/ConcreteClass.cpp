3c4e13d04b276822eef5fb0617d870c994357e4b
vigilantmaster <vigilantmster@gmail.com>
2018-07-17 13:01:10 -0700

    GOF_Template classes added

---
+++ CST276SRS02/GOF_Template/ConcreteClass.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "ConcreteClass.h"
+
+
+ConcreteClass::ConcreteClass()
+{
+}
+
+
+ConcreteClass::~ConcreteClass()
+{
+}
---
+++ CST276SRS02/GOF_Template/ConcreteClass.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "AbstractClass.h"
+class ConcreteClass :
+	public AbstractClass
+{
+public:
+	ConcreteClass();
+	~ConcreteClass();
+};
+
